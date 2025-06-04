40d105aec137780d6621560de7cd0dd293b94525
Breithner <Breithnerap@gmail.com>
2025-06-03 22:33:34 -0500

    add controllers for register from website

--- controllers/main.py
+++ controllers/main.py
@@ -14,17 +14,22 @@ class Main(http.Controller):
             'currencies': currencies
         })
 
+    
+    @http.route('/loan/submit', type='http', auth='public', website=True, methods=['POST'], csrf=True)
+    def loan_submit(self, **post):
+        try:
+            request.env['partner.loan'].sudo().create({
+                'partner_id': int(post.get('partner_id')),
+                'date_loan': post.get('date_loan'),
+                'amount_loan': float(post.get('amount_loan')),
+                'firts_date_payment': post.get('firts_date_payment'),
+                'currency_id': int(post.get('currency_id')) if post.get('currency_id') else None,
+                'note': post.get('note'),
+                'interest_rate': float(post.get('interest_rate', 0.0)),
+                'apply_interest': 'apply_interest' in post
+            })
+            
+            return request.redirect('/loan/thank-you')
 
-    # @http.route('/mod_bank_loan/objects', type='http', auth='public')
-    # def list(self, **kw):
-    #     return http.request.render(
-    #         'mod_bank_loan.listing',
-    #         {
-    #             'root': '/mod_bank_loan',
-    #             'objects': http.request.env['main'].search([]),
-    #         },
-    #     )
-
-    # @http.route('/mod_bank_loan/objects/<model("main"):obj>', auth='public')
-    # def object(self, obj, **kw):
-    #     return http.request.render('mod_bank_loan.object', {'object': obj})
+        except Exception as e:
+            return f"<h3>Error al crear el préstamo: {str(e)}</h3>"
